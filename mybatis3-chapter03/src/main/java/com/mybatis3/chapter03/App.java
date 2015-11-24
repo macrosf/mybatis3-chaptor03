040e83473e7b2115d5a37b1458290b6c05ce2203
macrosf@home <macros_f@163.com>
2015-11-24 21:38:34 +0800

    initialize

---
+++ mybatis3-chapter03/src/main/java/com/mybatis3/chapter03/App.java
@@ -0,0 +1,13 @@
+package com.mybatis3.chapter03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
